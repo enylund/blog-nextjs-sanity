0053defd868741f194e4ad336b702d2214d74662
Eric Nylund <ericnylund@erics-mbp.lan>
2023-05-20 13:06:53 -0400

    Work on page builder

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,3 +1,6 @@
+import cn from 'classnames'
+import { PortableText } from '@portabletext/react'
+import styles from './PostBody.module.css'
 import type { Post } from 'lib/sanity.queries'
 import { urlForImage } from 'lib/sanity.image'
 import Image from 'next/image'
@@ -7,13 +10,16 @@ export default function Hero(props: Pick<Post, 'heading' | 'tagline' | 'image'>)
   const { heading, tagline, image: source } = props
   return (
     <div>
-      This is an page Hero Component. The fields to render are {heading}, {tagline}
       <Image
+        className="h-auto w-full mb-8 sm:mx-0 md:mb-0"
         src={urlForImage(source).height(1000).width(2000).url()}
         width={2000}
         height={1000}
         alt="Picture of the author"
       />  
+      <div className={`md:mb-16 h-auto w-full mb-8  mx-auto max-w-2xl ${styles.portableText}`} >
+        {heading}, {tagline}
+      </div>
     </div>
   )
 }
